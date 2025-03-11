672e1a5431135e2c8c2eccafcccfff92035201bd
Wilfred Vásquez <wilfredvas@gmail.com>
2025-03-11 14:27:08 -0400

    Create module to link events with rooms. Show rooms into events

---
+++ event_booking_room/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    "name": "Enlace de eventos con Sala de Reuniones",  # str - required
+    "version": "0.0.1",  # str
+    "category": "Technical",  # str - default: Uncategorized
+    "summary": "Enlace de eventos con Sala de Reuniones",  # str
+    "description": """
+        Enlace de eventos con Sala de Reuniones
+    """,  # str,
+    "author": "Bitmotto",  # str
+    "company": "Bitmotto",  # str
+    "maintainer": "Bitmotto",  # str
+    "website": "https://www.bitmotto.com",
+    "depends": ["base", "event", "room"],  # list(str)
+    "data": ["views/event.xml"],  # list(str)
+    "license": "LGPL-3",  # str - default: LGPL-3
+    "installable": True,  # bool - default: True
+    "auto_install": False,  # bool - default: False
+    "application": False,  # bool - default: False
+}
